e2a63e9776a36b9aa60052542cba37c2622fec5b
ChengengLu <qq844303749@gmail.com>
2020-02-29 09:05:49 +0800

    version 2

--- src/main/kotlin/xyz/luchengeng/minio/starter/MinIOClient.kt
+++ src/main/kotlin/xyz/luchengeng/minio/starter/MinIOClient.kt
@@ -1,42 +1,56 @@
 package xyz.luchengeng.minio.starter
 import io.minio.ErrorCode
 import io.minio.MinioClient
-import io.minio.ServerSideEncryption
 import io.minio.errors.ErrorResponseException
-import org.apache.http.entity.ContentType
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
-import java.util.*
+import java.io.*
 
 class MinIOClient(private val client : MinioClient, private val props : MinIOProperties) {
     val raw : MinioClient
         get() = client
     val bucket : String
         get() = props.bucket
-    operator fun set(id : String,obj : Any){
-        if(obj is Unit){
-            client.removeObject(props.bucket,id)
+    operator fun set(id : String,obj : Any) {
+        if (obj is Unit) {
+            client.removeObject(props.bucket, id)
             return
         }
-        if(this.objectExists(id)){
-            client.removeObject(props.bucket,id)
+        if (this.objectExists(id)) {
+            client.removeObject(props.bucket, id)
         }
-        val baos = ByteArrayOutputStream()
-        val oos = ObjectOutputStream(baos)
-        oos.writeObject(obj)
-        oos.flush()
-        oos.close()
-        val inputStream = ByteArrayInputStream(baos.toByteArray())
-        client.putObject(props.bucket,id,inputStream,inputStream.available().toLong(),"application/octet-stream")
+        if (obj is MinIOObject) {
+            if (obj.contentType === null) {
+                client.putObject(props.bucket, id, serialize(obj), obj.headerMap)
+            } else if (obj.headerMap === null) {
+                client.putObject(props.bucket, id, serialize(obj), obj.contentType)
+            } else {
+                val stream = serialize(obj.obj)
+                client.putObject(
+                    props.bucket,
+                    id,
+                    stream,
+                    stream.available().toLong(),
+                    obj.headerMap,
+                    null,
+                    obj.contentType
+                )
+            }
+        } else {
+            val stream = serialize(obj)
+            client.putObject(
+                props.bucket,
+                id,
+                stream,
+                stream.available().toLong(),
+                "application/octet-stream"
+            )
+        }
     }
 
-    private fun objectExists(id : String) : Boolean{
-        return try{
-            client.statObject(props.bucket,id)
+    private fun objectExists(id: String): Boolean {
+        return try {
+            client.statObject(props.bucket, id)
             true
-        }catch (e : ErrorResponseException){
+        } catch (e: ErrorResponseException) {
             //potential bug
             if (e.errorResponse().errorCode().code() != ErrorCode.NO_SUCH_OBJECT.code()) {
                 throw e
@@ -45,8 +59,25 @@ class MinIOClient(private val client : MinioClient, private val props : MinIOPro
         }
     }
 
-    operator fun get(id : String) : Any {
-        if(!this.objectExists(id)) return Unit
-        return ObjectInputStream(client.getObject(props.bucket,id)).readObject()
+    operator fun get(id: String): Triple<Any, Map<String, List<String>>, String> {
+        return try {
+            val stat = client.statObject(props.bucket, id)
+            Triple(
+                ObjectInputStream(client.getObject(props.bucket, id)).readObject(),
+                stat.httpHeaders(),
+                stat.contentType()
+            )
+        } catch (e: ErrorResponseException) {
+            throw e
+        }
     }
+}
+
+fun serialize(obj: Any): InputStream {
+    val baos = ByteArrayOutputStream()
+    val oos = ObjectOutputStream(baos)
+    oos.writeObject(obj)
+    oos.flush()
+    oos.close()
+    return ByteArrayInputStream(baos.toByteArray())
 }
--- src/test/kotlin/xyz/luchengeng/minio/starter/test/MinIOStarterTest.kt
+++ src/test/kotlin/xyz/luchengeng/minio/starter/test/MinIOStarterTest.kt
@@ -1,12 +1,14 @@
 package xyz.luchengeng.minio.starter.test
 
 import org.junit.Test
+import org.junit.platform.commons.logging.LoggerFactory
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.core.env.Environment
 import org.springframework.test.context.junit4.SpringRunner
 import xyz.luchengeng.minio.starter.MinIOClient
+import xyz.luchengeng.minio.starter.withContentType
+import xyz.luchengeng.minio.starter.withHeader
 
 @SpringBootTest
 @RunWith(
@@ -14,14 +16,22 @@ import xyz.luchengeng.minio.starter.MinIOClient
 class MinIOStarterTest constructor() {
     @Autowired
     lateinit var client: MinIOClient
+    val logger = LoggerFactory.getLogger(MinIOStarterTest::class.java)
     @Test
-    fun test(){
-        client["str"] = "str"
-        assert(client["str"] == "str")
-        client["str"] = "str1"
-        assert(client["str"] == "str1")
-        client["str"] = Unit
-        assert(client["str"] is Unit)
-        assert(client["should return Unit"] is Unit)
+    fun test() {
+        client["test"] = Unit
+        client["pure"] = Unit
+        client["test"] = "test str" withHeader mutableMapOf("a" to "b") withContentType "application/json"
+        client["pure"] = "test str"
+        val (str, map, contentType) = client["test"]
+        for ((k, v) in map) {
+            logger.info { "$k to $v" }
+        }
+        val (str1, map1, contentType1) = client["pure"]
+        for ((k, v) in map1) {
+            logger.info { "$k to $v" }
+        }
+        assert(str == "test str")
+        assert(str1 == "test str")
     }
 }
